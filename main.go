dfa0d90de48dffeaa7fa68fb19ef20d1b1656d2f
Yang Xiaohui <yangxiaohui126@126.com>
2020-05-24 12:49:39 +0800

    add a space

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 
 func handlerFunc(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
+
 	fmt.Fprint(w, "<h1>Welcome to my super awesome site!</h1>")
 }
 
